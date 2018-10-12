25796e209a820619e6dc015faec049d814b5495a
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-10-12 13:05:27 -0400

    ex2.py

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,15 @@
 def reconstruct_trip(tickets):
-  pass 
+    flights = {}
+    planned_trip = []
+    for ticket in tickets:
+        flights[ticket[0]] = ticket[1] # setting the first ticket to be the second ticket when switching
+    active_ticket = flights[None] # default active ticket
+    while active_ticket != None:
+        planned_trip.append(active_ticket) # adding ticket to trip
+        if active_ticket not in flights: # no ticket returns empty array
+            return []
+        active_ticket = flights[active_ticket]
+    return planned_trip
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
